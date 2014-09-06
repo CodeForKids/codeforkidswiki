e34322574efb7b90321cdb226c06e219eed50246
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-09-06 19:21:55 -0400

    Errbit is secured now

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = ENV["AIRBRAKE_TOKEN"]
   config.host    = 'exceptions.codeforkids.ca'
-  config.port    = 80
-  config.secure  = config.port == 443
+  config.port    = 443
+  config.secure  = true
 end
