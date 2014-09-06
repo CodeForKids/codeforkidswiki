29b6f29eb1ff4b1503a2c13c7768f07803b82f78
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-09-06 18:51:43 -0400

    HTTP to HTTPS

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -11,7 +11,7 @@ class ApplicationHelperTest < ActionView::TestCase
   end
 
   test 'gravatar' do
-    assert_equal 'http://www.gravatar.com/avatar.php?default=&gravatar_id=7d88a4641924c30ad59ba00b200bbd5f&rating=&size=', gravatar('julian@codeforkids.ca')
+    assert_equal 'https://www.gravatar.com/avatar.php?default=&gravatar_id=7d88a4641924c30ad59ba00b200bbd5f&rating=&size=', gravatar('julian@codeforkids.ca')
   end
 
   test 'pluralize no count' do
